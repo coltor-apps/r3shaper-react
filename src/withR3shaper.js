25c4e2c2e1c4f5889e320fb20a2deabfe29e570d
masurceac <masurceac@gmail.com>
2019-11-17 17:43:47 +0200

    Fix: Pass options through HOC to hook

--- src/withR3shaper.js
+++ src/withR3shaper.js
@@ -5,9 +5,10 @@ import useR3shaper from './useR3shaper';
 export const withR3shaper = (
   OriginalComponent,
   resource,
-  resourceOptions
+  resourceOptions,
+  options
 ) => props => {
-  const r3shaperOptions = useR3shaper(resource, resourceOptions);
+  const r3shaperOptions = useR3shaper(resource, resourceOptions, options);
 
   return <OriginalComponent {...props} {...r3shaperOptions} />;
 };
